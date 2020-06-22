bd01b710508c0e438ba95d1ed5eb9dd601a5244b
akhila2akhila <akhila2akhila@gmail.com>
2020-06-22 10:22:00 -0400

    Initial Commit

---
+++ ImageCollector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageCollector
+//
+//  Created by Devi Krishna Lanka on 6/22/20.
+//  Copyright © 2020 udemy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
